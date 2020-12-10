8161c71885cbf74687864705f2a9ececb5594428
Evgeny Nenashev <nenashee@c02d60lmmd6q.corp.root.nasd.com>
2020-12-10 09:32:58 -0500

    Add initial project skeleton.

---
+++ test_pset.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `pb_etl` package."""
+
+from unittest import TestCase
+
+
+class FakeFileFailure(IOError):
+    pass
+
+
+class TestsStub(TestCase):
+    def basic_test_1(self):
+        pass
