e85892bcb75939351a8eeefc07ff9c11d11638a0
Evgeny Nenashev <evgeny.nenashev@gmail.com>
2020-12-13 10:56:31 -0500

    Tests

--- pb_etl/tasks.py
+++ pb_etl/tasks.py
@@ -55,8 +55,8 @@ cat_col = [
 ]
 num_col = attr_norm.copy()
 num_col.extend(["NS_V0", "NS_V1", "NS_V2"])
-s3_source_default = "s3://md-en-csci-e-29-final/"
-src_data_path = os.getenv("FINAL_PROJ_BUCKET", default=s3_source_default)
+# s3_source_default = "s3://md-en-csci-e-29-final/"
+# src_data_path = os.getenv("FINAL_PROJ_BUCKET", default=s3_source_default)
 
 
 def df_to_dataset(dataframe, shuffle=True, batch_size=32):
@@ -81,11 +81,15 @@ class ExtData(ExternalTask):
     data_source = ""
 
     def output(self):
-        pth = os.getenv(
-            "PSET5_PATH",
-            default=src_data_path + self.data_source,
-        )
-        return lt.CSVTarget(pth, storage_options=dict(requester_pays=True), flag=None)
+        src_data_path_default = "s3://md-en-csci-e-29-final/"
+        src_data_path = os.getenv("FINAL_PROJ_BUCKET" , default=src_data_path_default)
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!src_data_path: " + src_data_path + self.data_source)
+
+        # pth = os.getenv(
+        #     "PSET5_PATH",
+        #     default=self.src_data_path + self.data_source,
+        # )
+        return lt.CSVTarget(src_data_path + self.data_source, storage_options=dict(requester_pays=True), flag=None)
 
 
 class TrnAttr(ExtData):
